ad166d30f99daee97f15bb109050aec9158d3945
Scott Munson <sqmunson@gmail.com>
2016-03-07 20:55:24 -0500

    first commit, comment out some stuff on index and header

--- src/index.php
+++ src/index.php
@@ -4,16 +4,16 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+			<h1><?php // _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
-			<?php get_template_part('loop'); ?>
+			<?php // get_template_part('loop'); ?>
 
-			<?php get_template_part('pagination'); ?>
+			<?php // get_template_part('pagination'); ?>
 
 		</section>
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+<?php // get_sidebar(); ?>
 
-<?php get_footer(); ?>
+<?php // get_footer(); ?>
