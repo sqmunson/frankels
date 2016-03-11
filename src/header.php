3c6d0a4564ccba7009d3d528d4ad422c4036f9c2
Scott Munson <sqmunson@gmail.com>
2016-03-10 21:30:56 -0500

    add logo

--- src/header.php
+++ src/header.php
@@ -34,10 +34,8 @@
 
 					<!-- logo -->
 					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<!-- <img src="<?php // echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img"> -->
-						</a>
+						<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logo1.jpg" alt="Logo" class="logo-img">
 					</div>
 					<!-- /logo -->
 
